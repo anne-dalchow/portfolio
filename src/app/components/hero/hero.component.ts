72f9385c3821891fcb832f2c486ad31cd3850d12
Anne Dalchow <anne.dalchow@web.de>
2025-05-23 19:45:19 +0200

    create header component, add toggle language function

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
--- src/shared/components/header/header.component.ts
+++ src/shared/components/header/header.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ToggleLanguageComponent } from '../toggle-language/toggle-language.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, ToggleLanguageComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  language: 'en' | 'de' = 'en';
 
+  onLanguageChange(lang: 'en' | 'de') {
+    this.language = lang;
+    // Optional: Event oder Service zum Sprachwechsel (i18n)
+    console.log('Sprache gewechselt zu:', lang);
+  }
 }
