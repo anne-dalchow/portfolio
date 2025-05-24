f8f8dc157d183afb962fb7230d1774a477446030
Anne Dalchow <anne.dalchow@web.de>
2025-05-24 15:38:32 +0200

    import hover dot component

--- src/shared/components/header/header.component.ts
+++ src/shared/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { ToggleLanguageComponent } from '../toggle-language/toggle-language.component';
+import { HoverDotComponent } from '../hover-dot/hover-dot.component';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, ToggleLanguageComponent],
+  imports: [CommonModule, ToggleLanguageComponent, HoverDotComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
