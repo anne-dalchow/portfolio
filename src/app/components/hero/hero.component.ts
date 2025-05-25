e3b7435f27bb73d9f8fbe4862c2b0c36b95a695e
Anne Dalchow <anne.dalchow@web.de>
2025-05-25 13:46:52 +0200

    add Marquee Component for marquee banner and buttons

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../shared/components/header/header.component';
 import { HoverDotComponent } from '../../../shared/components/hover-dot/hover-dot.component';
 import { SlideButtonComponent } from '../../../shared/components/slide-button/slide-button.component';
+import { MarqueeComponent } from '../../../shared/components/marquee/marquee.component';
 
 @Component({
   selector: 'app-hero',
-  imports: [HeaderComponent, HoverDotComponent,SlideButtonComponent],
+  imports: [HeaderComponent,  MarqueeComponent,HoverDotComponent,SlideButtonComponent,],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
-
 }
