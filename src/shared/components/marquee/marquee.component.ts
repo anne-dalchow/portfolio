3a0b4f77b24fb87c679bede9b28ef573f8052f89
Anne Dalchow <anne.dalchow@web.de>
2025-05-25 13:49:21 +0200

    create shared component for marquees
    text slides from right to left

---
+++ src/shared/components/marquee/marquee.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import {
+  Component
+} from '@angular/core';
+
+@Component({
+  selector: 'app-marquee',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './marquee.component.html',
+  styleUrl: './marquee.component.scss'
+})
+export class MarqueeComponent{
+
+}
