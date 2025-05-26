2c67b744630d9bf93db42f4169c94677d910ebcd
Anne Dalchow <anne.dalchow@web.de>
2025-05-26 14:31:55 +0200

    Create skill-icons.component.spec.ts

---
+++ src/shared/components/skill-icons/skill-icons.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SkillIconsComponent } from './skill-icons.component';
+
+describe('SkillIconsComponent', () => {
+  let component: SkillIconsComponent;
+  let fixture: ComponentFixture<SkillIconsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [SkillIconsComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SkillIconsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
