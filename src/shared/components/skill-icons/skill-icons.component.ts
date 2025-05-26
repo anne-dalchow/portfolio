80ed3d6667b44af553a19df1a4d75cac00192888
Anne Dalchow <anne.dalchow@web.de>
2025-05-26 14:31:47 +0200

    Create skill-icons.component.ts

---
+++ src/shared/components/skill-icons/skill-icons.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-icons',
+  imports: [CommonModule],
+  templateUrl: './skill-icons.component.html',
+  styleUrl: './skill-icons.component.scss'
+})
+export class SkillIconsComponent {
+
+}
