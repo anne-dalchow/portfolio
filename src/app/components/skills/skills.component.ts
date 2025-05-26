071c32b9c634c4022c150fdc9fb02a5895e74dd0
Anne Dalchow <anne.dalchow@web.de>
2025-05-26 14:33:02 +0200

    add skillIconsComponent

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { InfoCardComponent } from '../info-card/info-card.component';
 import { SlideButtonComponent } from '../../../shared/components/slide-button/slide-button.component';
+import { SkillIconsComponent } from '../../../shared/components/skill-icons/skill-icons.component';
 
 @Component({
   selector: 'app-skills',
-  imports: [CommonModule, InfoCardComponent,SlideButtonComponent],
+  imports: [CommonModule, InfoCardComponent,SlideButtonComponent, SkillIconsComponent],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.scss'
 })
