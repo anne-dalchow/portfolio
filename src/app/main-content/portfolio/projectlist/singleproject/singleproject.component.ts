6a67eb41c2ad2a95303f35bc8da9beaf1be5bb54
Anne Dalchow <anne.dalchow@web.de>
2025-05-27 17:50:48 +0200

    relocate portfolio section to main-content file

---
+++ src/app/main-content/portfolio/projectlist/singleproject/singleproject.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singleproject',
+  imports: [],
+  templateUrl: './singleproject.component.html',
+  styleUrl: './singleproject.component.scss'
+})
+export class SingleprojectComponent {
+
+}
