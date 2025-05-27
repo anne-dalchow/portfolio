554273b241f06f4941aeeecc9330403b4d260238
Anne Dalchow <anne.dalchow@web.de>
2025-05-27 17:54:44 +0200

    refactor: move MainComponent logic out, keep footer and glow only

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeroComponent } from './components/hero/hero.component';
-import { AboutComponent } from './components/about/about.component';
-import { SkillsComponent } from './components/skills/skills.component';
-import { PortfolioComponent } from './components/portfolio/portfolio.component';
-import { TestimonialComponent } from './components/testimonial/testimonial.component';
-import { ContactComponent } from './components/contact/contact.component';
-import { FooterComponent } from '../shared/components/footer/footer.component';
 import { CursorGlowComponent } from '../shared/components/cursor-glow/cursor-glow.component';
+import { FooterComponent } from "../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,CommonModule,CursorGlowComponent, HeroComponent,AboutComponent,SkillsComponent,PortfolioComponent,TestimonialComponent,ContactComponent,FooterComponent],
+  imports: [RouterOutlet, CommonModule, CursorGlowComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
