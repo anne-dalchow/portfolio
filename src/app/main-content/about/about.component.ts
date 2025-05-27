847cdd4e89157efc41c072bc59a18ffe4b3fbb1f
Anne Dalchow <anne.dalchow@web.de>
2025-05-27 17:52:16 +0200

    relocateabout component sections to main-content file

---
+++ src/app/main-content/about/about.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { InfoCardComponent } from '../../../shared/components/info-card/info-card.component';
+
+
+@Component({
+  selector: 'app-about',
+  imports: [CommonModule, InfoCardComponent],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class AboutComponent {
+  isActivated = false;
+
+  activateOnce() {
+    if (!this.isActivated) {
+      this.isActivated = true;
+    }
+  }
+}
