844f09ef915fec76d11283b99144000448361072
Anne Dalchow <anne.dalchow@web.de>
2025-05-27 17:49:04 +0200

    relocate infoCard component for better structure

---
+++ src/shared/components/info-card/info-card.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-info-card',
+  imports: [CommonModule],
+  templateUrl: './info-card.component.html',
+  styleUrl: './info-card.component.scss'
+})
+export class InfoCardComponent {
+  @Input() title: string = '';
+  @Input() subtitle: string = '';
+@Input() maxwidth: number | string = '';
+
+}
