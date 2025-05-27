fc2cf5b0020c32c86f3fdbd107d762a5cc7d314d
Anne Dalchow <anne.dalchow@web.de>
2025-05-27 17:50:30 +0200

    relocate skill section to main-content file

---
+++ src/app/main-content/skills/skills.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { InfoCardComponent } from '../../../shared/components/info-card/info-card.component';
+import { SlideButtonComponent } from '../../../shared/components/slide-button/slide-button.component';
+import { SkillIconsComponent } from '../../../shared/components/skill-icons/skill-icons.component';
+
+@Component({
+  selector: 'app-skills',
+  imports: [CommonModule, InfoCardComponent,SlideButtonComponent, SkillIconsComponent],
+  templateUrl: './skills.component.html',
+  styleUrl: './skills.component.scss'
+})
+export class SkillsComponent {
+
+}
