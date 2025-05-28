ecedf1b7bc4f53ad13ed79278baa192a08912931
Anne Dalchow <anne.dalchow@web.de>
2025-05-28 17:49:50 +0200

    add index in projectlist

--- src/app/projectlistdata.service.ts
+++ src/app/projectlistdata.service.ts
@@ -10,6 +10,7 @@ export class ProjectlistdataService {
 
   projectlist = [
     {
+      index: 1,
       name: "Join",
       description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ",
       techStack: [
@@ -23,6 +24,7 @@ export class ProjectlistdataService {
       img: "/images/projects/join.jpg"
     },
     {
+      index: 2,
       name: "El Pollo Loco",
       description: "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ",
       techStack: [
@@ -34,6 +36,7 @@ export class ProjectlistdataService {
       img: "/images/projects/el_pollo_loco.jpg"
     },
     {
+      index: 3,
       name: "DA Bubble",
       description: "This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.",
       techStack: [
