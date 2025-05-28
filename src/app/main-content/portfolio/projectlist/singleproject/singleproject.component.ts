31356efe292e0cf71788150838ede97a95152c22
Anne Dalchow <anne.dalchow@web.de>
2025-05-28 13:29:11 +0200

    add project data

--- src/app/main-content/portfolio/projectlist/singleproject/singleproject.component.ts
+++ src/app/main-content/portfolio/projectlist/singleproject/singleproject.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-singleproject',
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './singleproject.component.scss'
 })
 export class SingleprojectComponent {
-
+  @Input() project = {
+    name: "Join",
+    description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ",
+    techStack: [
+      { name: "CSS", img: "/images/projects/CSS.svg" },
+      { name: "HTML", img: "/images/projects/HTML.svg" },
+      { name: "Firebase", img: "/images/projects/Firebase.svg" },
+      { name: "Angular", img: "/images/projects/Angular.svg" },
+      { name: "TypeScript", img: "/images/projects/TypeScript.svg" }
+    ],
+    previewImg: "/images/projects/join_preview.svg",
+    img: "/images/projects/join.jpg"
+  }
 }
