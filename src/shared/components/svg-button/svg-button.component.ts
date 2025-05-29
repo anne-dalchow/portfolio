1fdad2e01d8bde7abffd8e4c6dc478fd2823f982
Anne Dalchow <anne.dalchow@web.de>
2025-05-29 11:46:55 +0200

    create new component vor animated button style

---
+++ src/shared/components/svg-button/svg-button.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-button',
+  imports: [],
+  templateUrl: './svg-button.component.html',
+  styleUrl: './svg-button.component.scss'
+})
+export class SvgButtonComponent {
+
+}
