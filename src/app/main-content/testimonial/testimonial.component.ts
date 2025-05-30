fb733ea2bfefcd1725fd44b3ca69f0245d80e9a6
Anne Dalchow <anne.dalchow@web.de>
2025-05-30 15:44:58 +0200

    add methods for visiblity of single testimonial cards

--- src/app/main-content/testimonial/testimonial.component.ts
+++ src/app/main-content/testimonial/testimonial.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { SingleTestimonialComponent } from "./single-testimonial/single-testimonial.component";
+import { TestimonallistdataService } from '../../testimonallistdata.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-testimonial',
-  imports: [],
+  imports: [SingleTestimonialComponent, CommonModule],
   templateUrl: './testimonial.component.html',
   styleUrl: './testimonial.component.scss'
 })
 export class TestimonialComponent {
+
+  private readonly testimonallistdata = inject(TestimonallistdataService);
+  testimonials = this.testimonallistdata.testimonallist;
+
+  activeIndex = signal(0);
+
+  visibleTestimonials = computed(() => {
+    const list = this.testimonials;
+    const current = this.activeIndex();
+    const prev = (current - 1 + list.length) % list.length;
+    const next = (current + 1) % list.length;
+
+    return [list[prev], list[current], list[next]];
+  });
+
+  next() {
+    const max = this.testimonials.length;
+    this.activeIndex.update((i) => (i + 1) % max);
+  }
+
+  prev() {
+    const max = this.testimonials.length;
+    this.activeIndex.update((i) => (i - 1 + max) % max);
+  }
 
 }
