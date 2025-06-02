72db96ee90236c4c797498b6e00b2601d93c1eff
Anne Dalchow <anne.dalchow@web.de>
2025-06-02 20:15:57 +0200

    add HttpClient, add post onSubmit logic

---
+++ src/app/main-content/contact/contactform/contactform.component.ts
@@ -0,0 +1,55 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { SlideButtonComponent } from "../../../../shared/components/slide-button/slide-button.component";
+
+
+@Component({
+  selector: 'app-contactform',
+  imports: [FormsModule, CommonModule, SlideButtonComponent],
+  templateUrl: './contactform.component.html',
+  styleUrl: './contactform.component.scss'
+})
+export class ContactformComponent {
+
+  http = inject(HttpClient)
+
+  contactData = {
+    name: '',
+    email: '',
+    message: '',
+  }
+
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
+    }
+  }
+}
