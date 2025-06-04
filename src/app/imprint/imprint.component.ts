e3668f756fe451cd59059b5e821eecf8de018cfa
Anne Dalchow <anne.dalchow@web.de>
2025-06-04 16:55:50 +0200

    create imprint component, add content for html

---
+++ src/app/imprint/imprint.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/components/header/header.component";
+
+@Component({
+  selector: 'app-imprint',
+  standalone: true,
+  imports: [CommonModule, HeaderComponent],
+  templateUrl: './imprint.component.html',
+  styleUrl: './imprint.component.scss'
+})
+export class ImprintComponent {
+
+}
