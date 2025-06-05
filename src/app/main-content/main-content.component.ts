86573abb1078bbbde671d17c9b379eac668fc5dd
Anne Dalchow <anne.dalchow@web.de>
2025-06-05 15:35:17 +0200

    import carousel component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,12 +6,13 @@ import { TestimonialComponent } from "./testimonial/testimonial.component";
 import { ProjectlistComponent } from "./portfolio/projectlist/projectlist.component";
 import { ContactComponent } from "./contact/contact.component";
 import { LottieComponent } from "../lottie/lottie.component";
+import { CarouselComponent } from "../carousel/carousel.component";
 
 
 
 @Component({
   selector: 'app-main-content',
-  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectlistComponent, TestimonialComponent, ProjectlistComponent, ContactComponent, LottieComponent],
+  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectlistComponent, TestimonialComponent, ProjectlistComponent, ContactComponent, LottieComponent, CarouselComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
