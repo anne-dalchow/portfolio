83d76c42c52a96eefd4218d515e30bde11d61aff
Anne Dalchow <anne.dalchow@web.de>
2025-06-06 19:40:45 +0200

    update settings for TranslateService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,26 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CursorGlowComponent } from '../shared/components/cursor-glow/cursor-glow.component';
 import { FooterComponent } from "../shared/components/footer/footer.component";
+import {
+  TranslateService,
+  TranslatePipe,
+  TranslateDirective
+} from "@ngx-translate/core";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, CursorGlowComponent, FooterComponent],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, CursorGlowComponent, FooterComponent, TranslatePipe, TranslateDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(['de', 'en']);
+    this.translate.setDefaultLang('de');
+    this.translate.use('en');
+  }
   title = 'portfolio';
+
 }
