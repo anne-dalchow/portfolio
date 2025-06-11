97620cdc7043445bfe4266e2a340106bc938646c
Anne Dalchow <anne.dalchow@web.de>
2025-06-11 17:36:36 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImprintComponent } from './imprint/imprint.component';
-import { PrivacypoliceComponent } from '../pages/privacypolice/privacypolice.component';
+import { PrivacypoliceComponent } from './privacypolice/privacypolice.component';
 
 export const routes: Routes = [
  { path: '', component: MainContentComponent },
