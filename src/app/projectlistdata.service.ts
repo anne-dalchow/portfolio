9b8d349b7e4dd41daa655cf34972edb45e0686f9
Anne Dalchow <anne.dalchow@web.de>
2025-06-19 21:18:47 +0200

    add two more projects

--- src/app/projectlistdata.service.ts
+++ src/app/projectlistdata.service.ts
@@ -25,7 +25,9 @@ export class ProjectlistdataService {
         { name: "TypeScript", img: "images/projects/TypeScript.svg" }
       ],
       previewImg: "images/projects/join_preview.svg",
-      img: "images/projects/join.jpg"
+      img: "images/projects/join.jpg",
+      git: "https://github.com/anne-dalchow/join",
+      livetest: ""
     },
     {
       key: "el_pollo_loco",
@@ -38,13 +40,15 @@ export class ProjectlistdataService {
         { name: "JavaScript", img: "images/projects/Javascript.svg" },
       ],
       previewImg: "images/projects/el_pollo_loco_preview.svg",
-      img: "images/projects/el_pollo_loco.jpg"
+      img: "images/projects/el_pollo_loco.jpg",
+      git: "https://github.com/anne-dalchow/el-pollo-loco",
+      livetest: ""
     },
     {
       key: "da_bubble",
       index: 3,
       name: "DA Bubble",
-      description: "This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.",
+      description: "--- COMMING SOON --- This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.",
       techStack: [
         { name: "CSS", img: "images/projects/CSS.svg" },
         { name: "HTML", img: "images/projects/HTML.svg" },
@@ -52,6 +56,40 @@ export class ProjectlistdataService {
         { name: "TypeScript", img: "images/projects/TypeScript.svg" }
       ],
       previewImg: "images/projects/da_bubble_preview.svg",
-      img: "images/projects/slack_clone.jpg"
-    }];
+      img: "images/projects/slack_clone.jpg",
+      git: "https://github.com/anne-dalchow/",
+      livetest: ""
+    },
+    // {
+    //   key: "pokemon",
+    //   index: 4,
+    //   name: "Pokémon – Pokédex & Game",
+    //   description: "Explore a digital Pokédex and challenge friends in a turn-based card game. Featuring dynamic UI, API integration, and animated game logic for an engaging experience.",
+
+    //   techStack: [
+    //     { name: "CSS", img: "images/projects/CSS.svg" },
+    //     { name: "HTML", img: "images/projects/HTML.svg" },
+    //     { name: "JavaScript", img: "images/projects/Javascript.svg" },
+    //   ],
+    //   previewImg: "images/projects/da_bubble_preview.svg",
+    //   img: "images/projects/slack_clone.jpg",
+    //   git: "https://github.com/anne-dalchow/pokemon-cardDuell-pokedex-main",
+    //   livetest: ""
+    // },
+    // {
+    //   key: "mindfull_app",
+    //   index: 5,
+    //   name: "Mindfull App - Prototyping",
+    //   description: "Concept and prototype for a mindfulness and journaling app. Designed with a focus on calm colors, user guidance, and intuitive daily reflections.",
+
+    //   techStack: [
+    //     { name: "Figma", img: "images/skill-icons/figma.svg" },
+    //     { name: "Adobe Illustrator", img: "images/skill-icons/adobe_illustrator.svg" }
+    //   ],
+    //   previewImg: "images/projects/da_bubble_preview.svg",
+    //   img: "images/projects/slack_clone.jpg",
+    //   git: "",
+    //   livetest: ""
+    // }
+  ];
 }
