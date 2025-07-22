e833ce0fee10f5ce0eb5cdf858a8bcd2efe9e6bb
Anne Dalchow <anne.dalchow@web.de>
2025-07-22 13:17:52 +0200

    feat: allow passing a link to button component via input

--- src/shared/components/svg-button/svg-button.component.ts
+++ src/shared/components/svg-button/svg-button.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-svg-button',
+  selector: "app-svg-button",
   imports: [],
-  templateUrl: './svg-button.component.html',
-  styleUrl: './svg-button.component.scss'
+  templateUrl: "./svg-button.component.html",
+  styleUrl: "./svg-button.component.scss",
 })
 export class SvgButtonComponent {
-  @Input() text: string = '';
+  @Input() text: string = "";
+  @Input() link!: string;
+
+  openLink() {
+    if (this.link) {
+      window.open(this.link, "_blank", "noopener");
+    }
+  }
 }
