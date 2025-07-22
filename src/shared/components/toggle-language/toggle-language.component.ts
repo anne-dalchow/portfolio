46d296a5e4cb2c5ab279f92793e9e7da0fa18811
Anne Dalchow <anne.dalchow@web.de>
2025-07-22 13:16:56 +0200

    feat: load and store language preference from localStorage

--- src/shared/components/toggle-language/toggle-language.component.ts
+++ src/shared/components/toggle-language/toggle-language.component.ts
@@ -1,30 +1,34 @@
-import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
+import { CommonModule } from "@angular/common";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-toggle-language',
+  selector: "app-toggle-language",
   imports: [CommonModule],
-  templateUrl: './toggle-language.component.html',
-  styleUrl: './toggle-language.component.scss'
+  templateUrl: "./toggle-language.component.html",
+  styleUrl: "./toggle-language.component.scss",
 })
 export class ToggleLanguageComponent {
-  currentLang: 'en' | 'de' = 'en';
+  currentLang: "en" | "de" = "en";
 
   constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem("lang");
     const browserLang = navigator.language.slice(0, 2);
 
-    if (browserLang === 'de' || browserLang === 'en') {
+    if (savedLang === "de" || savedLang == "en") {
+      this.currentLang = savedLang as "en" | "de";
+    } else if (browserLang === "de" || browserLang === "en") {
       this.currentLang = browserLang;
     } else {
-      this.currentLang = 'en';
+      this.currentLang = "en";
     }
 
     this.translate.use(this.currentLang);
   }
 
-  toggleLanguage(language: 'en' | 'de') {
+  toggleLanguage(language: "en" | "de") {
     this.currentLang = language;
     this.translate.use(language);
+    localStorage.setItem("lang", language);
   }
 }
