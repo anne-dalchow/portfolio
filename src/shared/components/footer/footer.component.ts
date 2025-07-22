dccce137e7c7e017d34ea11e9c98e70e4a4712c8
Anne Dalchow <anne.dalchow@web.de>
2025-07-22 13:19:53 +0200

    feat: add scrollToTop method to logo-container img

--- src/shared/components/footer/footer.component.ts
+++ src/shared/components/footer/footer.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 import { HoverDotComponent } from "../hover-dot/hover-dot.component";
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-footer',
+  selector: "app-footer",
   imports: [RouterLink, HoverDotComponent, TranslatePipe],
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  templateUrl: "./footer.component.html",
+  styleUrl: "./footer.component.scss",
 })
 export class FooterComponent {
-
+  scrollToTop() {
+    const top = document.getElementById("top");
+    if (top) top.scrollIntoView({ behavior: "smooth" });
+  }
 }
