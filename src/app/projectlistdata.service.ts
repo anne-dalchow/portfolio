6e1ba8c85f15fe8139b4ce0df6540f9ab4b3662c
Anne Dalchow <anne.dalchow@web.de>
2025-07-22 13:20:24 +0200

    feat: add liveUrl and githubUrl to project data list

--- src/app/projectlistdata.service.ts
+++ src/app/projectlistdata.service.ts
@@ -1,95 +1,82 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class ProjectlistdataService {
   getProjectList() {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 
-  constructor() {
-  }
+  constructor() {}
 
   projectlist = [
     {
       key: "join",
       index: 1,
       name: "Join",
-      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ",
+      description:
+        "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ",
       techStack: [
         { name: "CSS", img: "images/projects/CSS.svg" },
         { name: "HTML", img: "images/projects/HTML.svg" },
         { name: "Firebase", img: "images/projects/Firebase.svg" },
         { name: "Angular", img: "images/projects/Angular.svg" },
-        { name: "TypeScript", img: "images/projects/TypeScript.svg" }
+        { name: "TypeScript", img: "images/projects/TypeScript.svg" },
       ],
       previewImg: "images/projects/join_preview.svg",
-      img: "images/projects/join.jpg",
-      git: "https://github.com/anne-dalchow/join",
-      livetest: ""
+      img: "images/projects/join_1.png",
+      githubUrl: "https://github.com/anne-dalchow/join",
+      liveUrl: "https://join.anne-dalchow.de/",
     },
     {
       key: "el_pollo_loco",
       index: 2,
       name: "El Pollo Loco",
-      description: "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ",
+      description:
+        "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen. ",
       techStack: [
         { name: "CSS", img: "images/projects/CSS.svg" },
         { name: "HTML", img: "images/projects/HTML.svg" },
         { name: "JavaScript", img: "images/projects/Javascript.svg" },
       ],
       previewImg: "images/projects/el_pollo_loco_preview.svg",
-      img: "images/projects/el_pollo_loco.jpg",
-      git: "https://github.com/anne-dalchow/el-pollo-loco",
-      livetest: ""
+      img: "images/projects/pollo_loco.png",
+      githubUrl: "https://github.com/anne-dalchow/el-pollo-loco",
+      liveUrl: "https://el-pollo-loco.anne-dalchow.de/",
     },
     {
-      key: "da_bubble",
+      key: "pokemon",
       index: 3,
-      name: "DA Bubble",
-      description: "--- COMMING SOON --- This App is a Slack Clone App. It revolutionizes team communication and collaboration with its intuitive interface, real-time messaging, and robust channel organization.",
+      name: "Pokémon – Pokédex & Game",
+      description:
+        "Explore a digital Pokédex and challenge friends in a turn-based card game. Featuring dynamic UI, API integration, and animated game logic for an engaging experience.",
+
       techStack: [
         { name: "CSS", img: "images/projects/CSS.svg" },
         { name: "HTML", img: "images/projects/HTML.svg" },
-        { name: "Angular", img: "images/projects/Angular.svg" },
-        { name: "TypeScript", img: "images/projects/TypeScript.svg" }
+        { name: "JavaScript", img: "images/projects/Javascript.svg" },
       ],
-      previewImg: "images/projects/da_bubble_preview.svg",
-      img: "images/projects/slack_clone.jpg",
-      git: "https://github.com/anne-dalchow/",
-      livetest: ""
+      previewImg: "images/projects/pokedex_preview.png",
+      img: "images/projects/pokedex.png",
+      githubUrl: "https://github.com/anne-dalchow/pokemon-cardDuell-pokedex-main",
+      liveUrl: "https://pokedex.anne-dalchow.de/",
     },
     // {
-    //   key: "pokemon",
+    //   key: "mindfull_app",
     //   index: 4,
-    //   name: "Pokémon – Pokédex & Game",
-    //   description: "Explore a digital Pokédex and challenge friends in a turn-based card game. Featuring dynamic UI, API integration, and animated game logic for an engaging experience.",
+    //   name: "Mindfull App - Prototyping",
+    //   description:
+    //     "Concept and prototype for a mindfulness and journaling app. Designed with a focus on calm colors, user guidance, and intuitive daily reflections.",
 
     //   techStack: [
     //     { name: "CSS", img: "images/projects/CSS.svg" },
     //     { name: "HTML", img: "images/projects/HTML.svg" },
-    //     { name: "JavaScript", img: "images/projects/Javascript.svg" },
     //   ],
     //   previewImg: "images/projects/da_bubble_preview.svg",
     //   img: "images/projects/slack_clone.jpg",
-    //   git: "https://github.com/anne-dalchow/pokemon-cardDuell-pokedex-main",
-    //   livetest: ""
+    //   githubUrl: "https://github.com/dein-repo",
+    //   liveUrl: "https://dein-projekt-url.com",
     // },
-    // {
-    //   key: "mindfull_app",
-    //   index: 5,
-    //   name: "Mindfull App - Prototyping",
-    //   description: "Concept and prototype for a mindfulness and journaling app. Designed with a focus on calm colors, user guidance, and intuitive daily reflections.",
-
-    //   techStack: [
-    //     { name: "Figma", img: "images/skill-icons/figma.svg" },
-    //     { name: "Adobe Illustrator", img: "images/skill-icons/adobe_illustrator.svg" }
-    //   ],
-    //   previewImg: "images/projects/da_bubble_preview.svg",
-    //   img: "images/projects/slack_clone.jpg",
-    //   git: "",
-    //   livetest: ""
-    // }
   ];
 }
