f324d2cd34271cd26553c6921e292b5cb70f5854
Anne Dalchow <anne.dalchow@web.de>
2025-07-22 13:31:24 +0200

    feat: scroll to top on route change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,27 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { CursorGlowComponent } from '../shared/components/cursor-glow/cursor-glow.component';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { Router, NavigationEnd, RouterOutlet } from "@angular/router";
+import { CursorGlowComponent } from "../shared/components/cursor-glow/cursor-glow.component";
 import { FooterComponent } from "../shared/components/footer/footer.component";
-import {
-  TranslateService,
-  TranslatePipe,
-  TranslateDirective
-} from "@ngx-translate/core";
-
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [RouterOutlet, CommonModule, CursorGlowComponent, FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  constructor(private translate: TranslateService) {
-    this.translate.addLangs(['de', 'en']);
-    this.translate.setDefaultLang('de');
-    this.translate.use('de');
+  constructor(private router: Router, private translate: TranslateService) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo({ top: 0 });
+      }
+    });
+    this.translate.addLangs(["de", "en"]);
+    this.translate.setDefaultLang("de");
+    this.translate.use("de");
   }
-  title = 'portfolio';
-
+  title = "portfolio";
 }
