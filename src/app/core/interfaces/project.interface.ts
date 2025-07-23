8c8d813d56117036e056864ce2dc63d54fb1696a
Anne Dalchow <anne.dalchow@web.de>
2025-07-23 15:56:03 +0200

    add interfaces

---
+++ src/app/core/interfaces/project.interface.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  key: string;
+  index: number;
+  techStack: { name: string; img: string }[];
+  previewImg: string;
+  img: string;
+  githubUrl: string;
+  liveUrl: string;
+}
---
+++ src/app/core/interfaces/testimonial.interface.ts
@@ -0,0 +1,5 @@
+export interface Testimonial {
+  key: string;
+  index: number;
+  name: string;
+}
