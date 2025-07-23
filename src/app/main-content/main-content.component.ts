6c75ab2884e2bbc48230f81147a17b35b722494e
Anne Dalchow <anne.dalchow@web.de>
2025-07-23 16:01:20 +0200

    add classname scroll-button & remove lottie component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 import { HeroComponent } from "./hero/hero.component";
 import { AboutComponent } from "./about/about.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { TestimonialComponent } from "./testimonial/testimonial.component";
 import { ProjectlistComponent } from "./portfolio/projectlist/projectlist.component";
 import { ContactComponent } from "./contact/contact.component";
-import { LottieComponent } from "../lottie/lottie.component";
 import { ScrollToggleButtonComponent } from "../../shared/components/scroll-toggle-button/scroll-toggle-button.component";
 
-
-
 @Component({
-  selector: 'app-main-content',
-  imports: [HeroComponent, AboutComponent, SkillsComponent, ProjectlistComponent, TestimonialComponent, ProjectlistComponent, ContactComponent, LottieComponent, ScrollToggleButtonComponent],
-  templateUrl: './main-content.component.html',
-  styleUrl: './main-content.component.scss'
+  selector: "app-main-content",
+  imports: [
+    HeroComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectlistComponent,
+    TestimonialComponent,
+    ProjectlistComponent,
+    ContactComponent,
+    ScrollToggleButtonComponent,
+  ],
+  templateUrl: "./main-content.component.html",
+  styleUrl: "./main-content.component.scss",
 })
-export class MainContentComponent {
-
-}
+export class MainContentComponent {}
