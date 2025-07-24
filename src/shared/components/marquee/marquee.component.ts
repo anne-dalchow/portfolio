aa49c31624be3dc34a778e8cf42c4b0a37c56679
Anne Dalchow <anne.dalchow@web.de>
2025-07-24 08:07:39 +0200

    Add i18n support for marquee text (EN/DE)

--- src/shared/components/marquee/marquee.component.ts
+++ src/shared/components/marquee/marquee.component.ts
@@ -1,15 +1,12 @@
-import { CommonModule } from '@angular/common';
-import {
-  Component
-} from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+import { TranslatePipe } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-marquee',
+  selector: "app-marquee",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './marquee.component.html',
-  styleUrl: './marquee.component.scss'
+  imports: [CommonModule, TranslatePipe],
+  templateUrl: "./marquee.component.html",
+  styleUrl: "./marquee.component.scss",
 })
-export class MarqueeComponent {
-
-}
+export class MarqueeComponent {}
